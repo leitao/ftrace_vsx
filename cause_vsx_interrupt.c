7f00b6e49e3f0cb2c55662d7c5809903ecd99502
Breno Leitao <breno.leitao@gmail.com>
2017-05-24 13:51:43 -0400

    Delaying to see 255 context switches

--- cause_vsx_interrupt.c
+++ cause_vsx_interrupt.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include<altivec.h>
+#include <unistd.h>
 
 int main(){
 	int i = 1024; 
@@ -9,6 +10,7 @@ int main(){
 	while (i--) {
 		bar = vec_add ((vector signed int){1, 2, 3, 4}, foo);
 		foo = vec_add (foo, (vector signed int) {2, 2, 2, 2});
+		usleep(1000);
 	}
 
 	printf("%d %d %d %d\n", foo[0], foo[1] , foo[2], foo[3]);
